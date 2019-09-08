a0a2eb48a9ca9191dac5d37cd8889cfa18241881
LordAlucard90 <khaufra.maggini@gmail.com>
2019-09-08 16:46:31 +0200

    imported project from spring initializer

---
+++ spring/databases/src/main/java/com/lordalucard90/howtotest/spring/databases/spring/databases/Application.java
@@ -0,0 +1,13 @@
+package com.lordalucard90.howtotest.spring.databases.spring.databases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
